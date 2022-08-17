c459b2bc14f2312e25a65bcf0a34984e214b48c0
aryanaman <amanaryankr@gmail.com>
2022-08-17 22:52:45 +0530

    added facebook clone in portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,23 +4,31 @@ import image1 from "../../assets/image1.jpg";
 import image2 from "../../assets/portfolioImg.jpg";
 import image3 from "../../assets/project_img2.jpg";
 import image4 from "../../assets/project_img4.jpg";
+import image5 from "../../assets/project_img5.JPG";
 const data = [
   {
-    id: 3,
-    image: image4,
-    title: "Tenzies Game",
-    github: "https://github.com/aryanaman/tenziesgame",
-    demo: "https://aryanaman.github.io/tenziesgame",
+    id: 1,
+    image: image5,
+    title: "Facebook Clone",
+    github: "https://github.com/aryanaman/facebook-clone",
+    demo: "https://facebook-clone-d1144.web.app/",
   },
   {
-    id: 1,
+    id: 2,
     image: image2,
     title: "My Website",
     github: "https://github.com/aryanaman/aman",
     demo: "https://aryanaman.github.io/aman/",
   },
   {
-    id: 2,
+    id: 4,
+    image: image4,
+    title: "Tenzies Game",
+    github: "https://github.com/aryanaman/tenziesgame",
+    demo: "https://aryanaman.github.io/tenziesgame",
+  },
+  {
+    id: 3,
     image: image3,
     title: "Weather App",
     github: "https://github.com/aryanaman/weather_app",
